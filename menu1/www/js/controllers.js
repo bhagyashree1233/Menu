556a321f81efff6d68bbda4a2d6a83b099bd38d5
bhagyashree1233 <bhags268@gmail.com>
2016-12-22 17:08:45 +0530

    Commit from git
    Commit from git

--- menu1/www/js/controllers.js
+++ menu1/www/js/controllers.js
@@ -1,6 +1,7 @@
 angular.module('starter.controllers', [])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+  //This File Edited from git
 //Edited Controller in git
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
